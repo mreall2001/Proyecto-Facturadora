96a4e9cba00a5256758c0e96fa0e45d0211a7675
JamesCQ <jamescaq@gmail.com>
2020-04-01 19:33:42 +0200

    simplon

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Facturadora</title>    
+<style>
+    .titulo {
+        text-align: center;
+    }
+</style>
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<body>
+    <div class="titulo">
+        <h1>Factura<h1>
+    </div>        
+    <div class="container row col text-center">
+        <button type="button" class="btn btn-primary btn-lg btn-block" href="nueva.php">Crear factura</button>
+        <button type="button" class="btn btn-primary btn-lg btn-block" href="listado.php">Listado</button>
+    </div>
+</body>
+</html>
