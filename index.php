1f64ae4637a0700da9644aeae0c87ac0953f6d3f
JamesCQ <jamescaq@gmail.com>
2020-04-21 19:39:05 +0200

    Final

--- index.php
+++ index.php
@@ -1,72 +1,50 @@
-<!DOCTYPE html>
 <html>
 <head>
-<title>Facturadora</title>    
-<style>
-    .titulo {
-        text-align: center;
-    }
-    .btn-primary {
-        margin: 20px 10px;
-    }
-    .titulo {
-
-    }
-    .container {
-        border: 1px solid black;
-        left: 15px;
-        border-style: dotted;
-        border-width: thick;
-    }
-    th {
-        text-align: center;
-        padding: 260px;
-    }
-    p {
-        font-style: italic;
-    }
-    .foto {
-        margin-left: 200px;
-        margin-right: 10px;
-    }
-    .gmaster {
-        width: 150px;
-        height: 150px;
-        margin-right: 350px;
-        margin-left: 60px;
-        bottom: 100%;
-        padding: -100px;
-        margin-top: 0px;
-        margin-bottom: -300px;
-    }
-</style>
+<title>Revenge of the Fallen</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="indexcss.css" />
 </head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <body>
-    <div class="titulo">
-        <h1>Facturadora</h1>
-    </div>  
-        <div class="container row col text-center">
-            <a type="button" class="btn btn-primary btn-lg btn-block" href="nueva.php">Crear factura</a>
-            <a type="button" class="btn btn-primary btn-lg btn-block" href="listado.php">Listado</a>
-        </div><br>
-    <div class="titulo">
-        <h2>Creditos</h2>
-    </div>
-    <div class="foto">
-        <img src="gmaster.jfif" alt="Foto" class="gmaster">
-        <img src="gmaster.jfif" alt="Foto" class="gmaster">
-        <img src="gmaster.jfif" alt="Foto" class="gmaster">
-    </div>
-    <table>
-        <tr>
-            <th>(Programador)<br>
-        <p>Miguel Real López</p></th>
-            <th>(Programador)<br>
-        <p>James Cañarte Quintana</p></th>
-            <th>(Programador)<br>
-        <p>Edgard Leandro Ocampo</p></th>
-        </tr>
-    </table>
+<footer>
+<a href="nueva.php" class="button special" style="width:100%;">Crear Factura</a>
+</footer>
+</div>
+</section>
+
+<section id="two" class="wrapper style1 special">
+<div class="inner">
+<header>
+<h2>Programadores</h2>
+<p></p>
+</header>
+<div class="flex flex-3">
+<div class="box person" style="min-width:33%">
+<div class="image round">
+<img src="challenger.jfif" alt="Skilly" />
+</div>
+<h3>James Cañarte Quintana</h3>
+<p>
+<div style="font-style:italic">
+</div>
+</p>
+</div>
+<div class="box person" style="min-width:33%">
+<div class="image round">
+<img src="challenger.jfif" alt="MIKEEEEEEEEEEEEE" />
+</div>
+<h3>Miguel Real López</h3>
+<p>
+</p>
+</div>
+<div class="box person" style="min-width:33%">
+<div class="image round">
+<img src="challenger.jfif" alt="Miniguy" />
+</div>
+<h3>Edgard Leandro Ocampo</h3>
+<p>
+</p>
+</div>
+</div>
+<br><br><br>
 </body>
 </html>
